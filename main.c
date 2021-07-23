44c5d55cf96b51957a4af8c5c3898d373059755b
Mateusz Szymiec <mattszymiec@gmail.com>
2021-07-24 01:28:33 +0200

    Add project files

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: matts
+ *
+ * Created on July 24, 2021, 1:23 AM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
