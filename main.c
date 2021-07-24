76690123d3cd8e93bdb6bb49db896183a861e9e5
Mateusz Szymiec <mattszymiec@gmail.com>
2021-07-24 02:00:28 +0200

    Add .gitignore file

--- main.c
+++ main.c
@@ -5,11 +5,19 @@
  * Created on July 24, 2021, 1:23 AM
  */
 
+#define F_CPU 1000000UL
 
+//#include <xc.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
-    /* Replace with your application code */
+    DDRD = 1 << DDB1;
+    
     while (1) {
+        PORTB = 1 << PB1;
+        _delay_ms(100);
+        PORTB = 0 << PB1;
+        _delay_ms(100);
     }
 }
